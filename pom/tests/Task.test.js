f29447505f2c578221c0db952584e73a5b20ae72
Luis Cuauhtemoc Montoya <luis.cuauhtemoc@luiscuauhtemoc.local>
2021-11-04 17:26:26 -0600

    Smoke Test and Visual Reports implemented

--- pom/tests/Task.test.js
+++ pom/tests/Task.test.js
@@ -9,24 +9,24 @@ fixture('Tasks tests')
     await t.useRole(STANDARD_USER)
 })
 
-test.meta({smokeTest:true})('As a user, I should be able to add a new Task with Today as the due date', async t => {
+test('As a user, I should be able to add a new Task with Today as the due date', async t => {
     await mainPage.addMultipleTasks(TASKS.TASKS_INFO.NAME_OF_TASK, 1)
  })
 
 
- test('As a user, I should be able to add a new Task with Tomorrow as the due date', async t => {
+ test.meta('type','smoke')('As a user, I should be able to add a new Task with Tomorrow as the due date', async t => {
     await mainPage.addTaskDueTomorrow()
- }).meta({ smokeTest:true})
+ })
 
- test.skip('As a user, I should be able to add multiple Tasks with Today as the due date', async t => {
+ test('As a user, I should be able to add multiple Tasks with Today as the due date', async t => {
     await mainPage.addMultipleTasks(TASKS.TASKS_INFO.NAME_OF_TASK, TASKS.TASKS_INFO.NUMBER_OF_TASKS)
- }).meta({ smokeTest: false })
+ })
 
- test('As a user, I should be able to add a new Project', async t => {
+ test.meta('type','smoke')('As a user, I should be able to add a new Project', async t => {
     await mainPage.createNewProject()
  })
 
- test('As a user, I should be able to delete all of the tasks', async t => {
+ test.meta('type','smoke')('As a user, I should be able to delete all of the tasks', async t => {
     await mainPage.deleteTasks()
  })
 
